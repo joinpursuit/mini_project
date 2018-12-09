5eaae16a67da4bca024332211d0ac27d207ee2bc
your name here <pursuit@MacBook-Air-3.local>
2018-12-08 19:15:53 -0500

    each list name clickable but still incomplete

--- frontend/script.js
+++ frontend/script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let usersButton = document.querySelector('#users-button');
   let usersDiv = document.querySelector('#users');
   let ul = document.querySelector('#users-list');
+  let clickedUser = document.querySelector('#singleUser');
 
   let photosButton = document.querySelector('#photos-button');
   let photosDiv = document.querySelector('#photos');
@@ -15,6 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let postsButton = document.querySelector('#posts-button');
   let postsDiv = document.querySelector('#posts');
   let postsList = document.querySelector('#plist');
+
+  let users = {};
+  let photos = {};
+  let posts = {};
 
   const hideDisplay = () => {
     tabcontent.forEach(el => {
@@ -28,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const displayUsers = () => {
     axios.get('http://localhost:3000/user')
     .then(res => {
+      users = res.data.users;
       res.data.users.forEach(el => {
         let li = document.createElement('li');
         li.innerText = el.name;
@@ -39,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const displayPosts = () => {
     axios.get('http://localhost:3000/post')
     .then(res => {
+      posts = res.data.posts;
       res.data.posts.forEach(el => {
         let p = document.createElement('p');
         p.innerText = el.body;
@@ -50,6 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const displayPhotos = () => {
     axios.get('http://localhost:3000/picture')
     .then(res => {
+      photos = res.data.photos;
       res.data.photos.forEach(el => {
         let img = document.createElement('img');
         img.setAttribute('src', el.url);
@@ -82,6 +90,22 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   ul.addEventListener('click', (event) => {
-
+    clickedUser.innerText = '';
+    users.forEach((el, i) => {
+      if (el.name === event.target.innerText) {
+        let img = document.createElement('img');
+        img.setAttribute('src', photos[i].url);
+        let name = document.createElement('h2');
+        name.innerText = el.name;
+        let age = document.createElement('h3');
+        age.innerText = el.age;
+        let post = document.createElement('p');
+        post.innerText = posts[i].body;
+        clickedUser.appendChild(img);
+        clickedUser.appendChild(name);
+        clickedUser.appendChild(age);
+        clickedUser.appendChild(post);
+      }
+    })
   })
 });
