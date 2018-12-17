4aa28520108d2da3fada82953b50954cd9e2dfa0
Nielene Thomas <nielenethomas@pursuit.org>
2018-12-17 08:36:04 -0500

    list of users background

--- frontend/script.js
+++ frontend/script.js
@@ -9,18 +9,42 @@ document.addEventListener('DOMContentLoaded', () => {
   .then(res => {
     return res.json();
   }).then(parsed => {
+
+
+
+    // let usersDivBackground = document.createElement('div');
+    // usersDivBackground.classList.add('usersDivBackground')
+    // body.appendChild(usersDivBackground);
+
+
+    let usersDiv = document.createElement('div');
+    usersDiv.classList.add('usersDiv')
+    body.appendChild(usersDiv);
+
     let h1 = document.createElement('h1');
     h1.innerText = "List of Users";
-    body.appendChild(h1);
+    usersDiv.appendChild(h1);
+
 
     parsed.listOfUsers.forEach(user => {
       let newButton = document.createElement('button');
       let br = document.createElement('br')
       newButton.innerText = user.name;
+      newButton.classList.add('userNameButton')
 
-      body.appendChild(newButton);
-      body.appendChild(br);
-    })
-  })
+      usersDiv.appendChild(newButton);
+      usersDiv.appendChild(br);
+    }) // end of forEach.
+
+    let searchInputDiv = document.createElement('div');
+    searchInputDiv.classList.add('searchInputDiv');
+    body.appendChild(searchInputDiv);
+
+    let searchInput = document.createElement('input');
+    searchInput.classList.add('search');
+
+
+
+  }) // end of parsed.
 
 })
